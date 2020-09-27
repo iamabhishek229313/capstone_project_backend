5dc819cde1876847f876ae2f9ed4832e17aa4571
Abhishek Kumar <iamabhishek229313@gmail.com>
2020-09-27 21:51:36 +0530

    Path params

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Optional
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -12,3 +13,29 @@ async def read_root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/user/me")
+async def read_user_me():
+    return {"current_user": "This user is me!"}
+
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"current_user": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/model/{model_name}")
+async def read_model_name(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"Model name is:": model_name, "message": "Deep Learning FTW!"}
+    elif model_name == ModelName.lenet:
+        return {"Model name is:": model_name, "message": "LeCNN all the images"}
+    else:
+        return {"Model name is:": model_name, "message": "Have some residuals"}
